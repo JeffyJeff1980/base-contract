fb20ecf441e6d35596abf9bd7e9c8d94f03c52a7
Jean-Francois Beaulieu <jfpb1980@gmail.com>
2022-12-30 21:03:00 -0500

    added hardhat and fixed mionor bug

--- contract/IBaseContract.sol
+++ contract/IBaseContract.sol
@@ -17,5 +17,5 @@ interface IBaseContract {
   function supportsInterface(bytes4 interfaceId) external view returns (bool);
   function tokenURI(uint256 tokenId) external view returns (string memory);
   function withdraw() external;
-  function withdrawToken(address tokenContractAddress, uint256 amount)
+  function withdrawToken(address tokenContractAddress, uint256 amount) external;
 }
